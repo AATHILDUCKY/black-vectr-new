aef70b32766639b3e78002c2a65472b06584541f
DUCKY <aathilducky@gmail.com>
2026-07-11 19:34:35 +0530

    update generated site content

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'bvec-1848db0715e5';
+const CACHE = 'bvec-2713cb203f6a';
 const BASE = self.location.pathname.replace(/\/sw\.js$/, '') || '';
-const MD_CACHE = 'bvec-md-1848db0715e5';
+const MD_CACHE = 'bvec-md-2713cb203f6a';
 
 const CODE_EXT = /\.(js|css)$/;
 const STATIC_EXT = /\.(jpg|jpeg|png|gif|webp|svg|woff2?|ttf|eot)$/;
